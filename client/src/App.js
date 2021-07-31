deb4fcebef654b9d776da40828101234d684797b
FedericoMelgarejo <f.n.melgarejo@gmail.com>
2021-07-30 23:40:13 -0300

    added imports to app.js and fixed some errors on package.json

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,10 @@
 import React from "react";
+import Incomes from "./components/incomes";
+import Expenses from "./components/expenses";
+import Balance from "./components/balance";
+import Navbar from "./components/navbar";
+import { Row, Col, Container} from "react-bootstrap";
+
 
 
 function App() {
